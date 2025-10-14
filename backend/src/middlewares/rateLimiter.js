1db11f46c493de8dfc08b2ce8bf8213f797acb71
Hafidzrdwn <hafidz10032005@gmail.com>
2025-10-14 20:31:43 +0700

    fix: update rate limiter configuration to allow more requests within a shorter time frame

--- backend/src/middlewares/rateLimiter.js
+++ backend/src/middlewares/rateLimiter.js
@@ -1,8 +1,8 @@
 import rateLimit from 'express-rate-limit';
 
 export const apiLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 10,
+  windowMs: 10 * 60 * 1000,
+  max: 150,
   message: { message: 'Terlalu banyak request, silakan coba lagi nanti.' },
   standardHeaders: true,
   legacyHeaders: false,
