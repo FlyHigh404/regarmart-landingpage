52e9c4764d9404ef1b2e54e9ade9f1acb6134c3d
Riska <riskafebriana168@gmail.com>
2025-10-16 16:12:05 +0700

    menyambungkan ke backend bagian faqdan category

---
+++ frontend/src/api/faq.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:5000/api";
+
+export const getFaqs = async () => {
+  try {
+    const response = await axios.get($, { BASE_URL } / faqs);
+
+    if (response.data.success && Array.isArray(response.data.data)) {
+      return response.data.data;
+    }
+
+    console.error("API response data is not an array:", response.data);
+    return [];
+  } catch (error) {
+    console.error("Error fetching FAQs:", error);
+
+    throw new Error("Gagal mengambil data FAQ dari server.");
+  }
+};
