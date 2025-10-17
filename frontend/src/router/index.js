0e97c1ee471d88c61b9d4f145c3e3cae40308796
Hafidzrdwn <hafidz10032005@gmail.com>
2025-10-17 17:07:13 +0700

    refactor: standardize quotation marks and improve scrollBehavior function

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,38 +1,38 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: () => import('@/views/Home.vue'),
+    path: "/",
+    name: "Home",
+    component: () => import("@/views/Home.vue"),
   },
   {
-    path: '/katalog',
-    name: 'Katalog',
-    component: () => import('@/views/Katalog.vue'),
+    path: "/katalog",
+    name: "Katalog",
+    component: () => import("@/views/Katalog.vue"),
   },
   {
-    path: '/tentang',
-    name: 'Tentang',
-    component: () => import('@/views/Tentang.vue'),
+    path: "/tentang",
+    name: "Tentang",
+    component: () => import("@/views/Tentang.vue"),
   },
   {
-    path: '/testimoni',
-    name: 'Testimoni',
-    component: () => import('@/views/Testimoni.vue'),
+    path: "/testimoni",
+    name: "Testimoni",
+    component: () => import("@/views/Testimoni.vue"),
   },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition
-    } else {
-      return { top: 0, left: 0 }
-    }
-  },
-})
+    if (savedPosition) return savedPosition;
 
-export default router
+    if (to.hash) return document.querySelector(to.hash).scrollIntoView({ behavior: 'smooth' });
+
+    return { top: 0, left: 0 };
+  },
+});
+
+export default router;
