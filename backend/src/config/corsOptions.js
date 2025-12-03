25bc3d8db5e8b7e489a5943a125ad3fa012470ed
sadrakhxvid777@gmail.com <sadrakhxvid777@gmail.com>
2025-12-03 18:29:00 +0700

    add: process.env.frontend_url

--- backend/src/config/corsOptions.js
+++ backend/src/config/corsOptions.js
@@ -1,4 +1,4 @@
-const allowedOrigins = ["http://localhost:3000", "http://localhost:5173"];
+const allowedOrigins = ["http://localhost:3000", "http://localhost:5173", process.env.FRONTEND_URL];
 
 const corsOptions = {
   origin: (origin, callback) => {
