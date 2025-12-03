1c8a8f41cfd396c269b06088933cc6004cf793a5
sadrakhxvid777@gmail.com <sadrakhxvid777@gmail.com>
2025-12-03 11:20:32 +0700

    add: path, sendfile to index.html to deploy

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import cookieParser from "cookie-parser";
+import path from 'path';
+import { fileURLToPath } from 'url';
 import ApiError from './utils/apiError.js';
 import { globalErrorHandler } from './middlewares/errorHandler.js';
 import httpStatus from 'http-status';
@@ -14,14 +16,20 @@ import swaggerSpec from './config/swagger.js';
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(helmet());
 app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, 'dist')));
+
 app.get('/', (req, res) => {
-  res.send('API is running..., please use /api for main routes');
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  // res.send('API is running..., please use /api for main routes');
 });
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
@@ -31,6 +39,10 @@ app.use('/api', apiRoutes);
 // handle 404
 app.use((req, res, next) => {
   next(new ApiError(httpStatus.NOT_FOUND, 'Endpoint tidak ditemukan'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.use(globalErrorHandler);
