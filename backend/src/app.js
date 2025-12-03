b6d0f4eacfdd3fd635a64ab6604a644701fc2216
sadrakhxvid777@gmail.com <sadrakhxvid777@gmail.com>
2025-12-03 19:30:08 +0700

    mod: line 44 from "*" to "/*"

--- backend/src/app.js
+++ backend/src/app.js
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
   next(new ApiError(httpStatus.NOT_FOUND, 'Endpoint tidak ditemukan'));
 });
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
