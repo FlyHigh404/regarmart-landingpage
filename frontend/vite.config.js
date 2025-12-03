38e38daf985999be2355db2aab10b0666423988f
sadrakhxvid777@gmail.com <sadrakhxvid777@gmail.com>
2025-12-03 11:22:01 +0700

    mod: remove servers ngrok

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,4 +13,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'), // sekarang '@' menunjuk ke folder src
     },
   },
+  // server: {
+  //   host: '0.0.0.0',      // supaya bisa diakses dari luar
+  //   port: 5173,           // sesuaikan dengan port project kamu
+  //   allowedHosts: [
+  //     '784d89982bdf.ngrok-free.app' // ganti dengan domain ngrok kamu
+  //   ]
+  // }
 })
